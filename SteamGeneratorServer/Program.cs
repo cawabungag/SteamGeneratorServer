fa0dc240e01791d157973d53294efdc67425f3be
Ilyas Ilyasov <38839317+cawabungag@users.noreply.github.com>
2023-05-09 19:54:02 +0300

    implemented rest api

--- SteamGeneratorServer/Program.cs
+++ SteamGeneratorServer/Program.cs
@@ -1,5 +1,6 @@
 using DailySpendingBot.Repositories;
 using FinanceService.Controllers;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 var services = builder.Services;
@@ -8,5 +9,13 @@ services.AddSingleton<StateController>();
 services.AddSingleton(new DatabaseHandler(contentRootPath));
 services.AddSingleton<IStateRepository, StateRepository>();
 services.AddSingleton<IMeasureRepository, MeasureRepository>();
+services.AddControllers();
+
+services.AddSwaggerGen(c
+	=> c.SwaggerDoc("v1", new OpenApiInfo {Title = "Steam Generator Server", Version = "v1"}));
+
 var app = builder.Build();
+app.UseSwagger();
+app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1"));
+
 app.Run();
